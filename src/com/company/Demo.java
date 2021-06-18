a68ec75ff16db1c4fd3ffb2a8d3cc8b40d54a667
Vasily-Borzykh <vasyvoin@mail.ru>
2021-06-18 17:39:44 +0300

    Initial commit

--- src/com/company/Demo.java
+++ src/com/company/Demo.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Main {
+public class Demo {
     public static void main(String[] args) {
         Editor editor = new Editor();
         editor.events.subscribe("open", new LogOpenListener("/path/to/log/file.txt"));
